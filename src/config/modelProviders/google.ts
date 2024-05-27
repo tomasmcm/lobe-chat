391f5cea8b25c92ffa95c7de691cbe1732404b58
tomasmcm <tomasmcm@whitesmith.co>
2024-05-27 11:51:55 +0100

    add prices

--- src/config/modelProviders/google.ts
+++ src/config/modelProviders/google.ts
@@ -3,21 +3,23 @@ import { ModelProviderCard } from '@/types/llm';
 const Google: ModelProviderCard = {
   chatModels: [
     {
-      description: 'The best image understanding model to handle a broad range of applications',
-      displayName: 'Gemini 1.0 Pro Vision',
-      id: 'gemini-1.0-pro-vision',
-      maxOutput: 4096,
-      private: false,
-      tokens: 12_288 + 4096,
-      vision: true,
-    },
-    {
       description: 'The best model for scaling across a wide range of tasks.',
       displayName: 'Gemini 1.0 Pro',
       id: 'gemini-1.0-pro',
       maxOutput: 2048,
+      price: '€0.23',
       private: false,
       tokens: 30_720 + 2048,
+    },
+    {
+      description: 'The best image understanding model to handle a broad range of applications',
+      displayName: 'Gemini 1.0 Pro Vision',
+      id: 'gemini-1.0-pro-vision',
+      maxOutput: 4096,
+      price: '€0.23',
+      private: false,
+      tokens: 12_288 + 4096,
+      vision: true,
     },
     {
       description: 'Fast and versatile multimodal model for scaling across diverse tasks',
@@ -25,6 +27,7 @@ const Google: ModelProviderCard = {
       functionCall: true,
       id: 'gemini-1.5-flash',
       maxOutput: 8192,
+      price: '€0.23',
       private: false,
       tokens: 1_048_576 + 8192,
       vision: true,
@@ -35,6 +38,7 @@ const Google: ModelProviderCard = {
       functionCall: true,
       id: 'gemini-1.5-pro',
       maxOutput: 8192,
+      price: '€2.30',
       private: false,
       tokens: 1_048_576 + 8192,
       vision: true,
